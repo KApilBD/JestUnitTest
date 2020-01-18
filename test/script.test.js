efeb699b87b4b131fc60190a68fee4ce32269c8a
KApilBD <kapilbaraskar91@gmail.com>
2020-01-18 15:55:41 +0530

    Mock Api call

--- test/script.test.js
+++ test/script.test.js
@@ -12,7 +12,7 @@ const swapi = require('./script');
 it('calls swapi to get people', () => {
     expect.assertions(1);
     return swapi.getPeople(fetch).then(data => {
-        expect(data.count).toEqual(87); 
+        expect(data.count).toEqual(87);
     })
 })
 
@@ -23,4 +23,20 @@ it('calls swapi to get people with promise', () => {
         expect(data.count).toEqual(87);
         expect(data.results.length).toBeGreaterThan(5)
     })
+})
+
+
+it('get people returns count and results', () => {
+    const mockFetch = jest.fn()
+        .mockReturnValue(Promise.resolve({
+            json: () => Promise.resolve({
+                count: 87,
+                results: [0, 1, 2, 3, 4, 5, 6]
+            })
+        }))
+        expect.assertions(2);
+        return swapi.getPeoplePromise(mockFetch).then(data => {
+            expect(mockFetch.mock.calls.length).toBe(1);
+            expect(mockFetch).toBeCalledWith("https://swapi.co/api/people");
+        })
 })
